4c1772e4f70fdedb5d429642d760b313f0bbe4be
danielamartinezok <85463949+danielamartinezok@users.noreply.github.com>
2023-02-18 15:07:38 -0300

    Clases
    Clases: Cliente, Factura, App y Program

---
+++ App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase16_02
+{
+    class App
+    {
+        private Cliente[] clientes;
+        private Factura[] facturas;
+
+        public App() 
+        {
+            clientes = new Cliente[100];
+            facturas= new Factura[1000];
+        }
+
+        public void ejecutar()
+        {
+            string opcion = "";
+            do
+            {
+                opcion = pedirStringNoVacio("Menú:\nA - Ingresar cliente\nB - Ingresar Factura\nListar facturas\nD - Estadísticas\nE - Salir");
+
+                if (opcion.Equals("A"))
+                {
+                    altaDeClientes();
+                }
+                else if (opcion.Equals("B"))
+                {
+                    altaDeFacturas();
+                }
+                else if (opcion.Equals("C"))
+                {
+                    listarFacturas();
+                }
+                else if (opcion.Equals("D"))
+                {
+                    mostrarEstadisticas();
+                }
+
+            } while (opcion != "E");
+        }
+    }
+}
---
+++ Factura.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase16_02
+{
+    class Factura
+    {
+        private string cliente;
+        private double importe;
+        private long numero;
+
+        public Factura(string c, double i, long n)
+        {
+            this.cliente = c;
+            this.importe = i;
+            this.numero = n;
+        }
+
+        public override bool Equals(object otraFactura)
+        {
+            bool retorno = false;
+
+            if (otraFactura != null)
+            {
+                if (otraFactura is Factura)
+                {
+                    Factura compararFactura = otraFactura as Factura;
+
+                    if (compararFactura.numero == this.numero)
+                    {
+                        retorno = true;
+                    }
+                }
+            }
+            return retorno;
+        }
+
+        public double getImporte()
+        {
+            return importe;
+        }
+    }
+}
